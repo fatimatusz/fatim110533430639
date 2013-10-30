426743e058fedb6b25046beab742541f420e106e
fatimatusz <zafa_violet@yahoo.com>
2013-10-30 14:23:08 +0700

    fatimatuszahroh_110533430639_modul4

---
+++ lat 1.php
@@ -0,0 +1,31 @@
+<?php
+//  Men-set  nilai  cookie
+setcookie('nama_cookie',  'nilai_cookie');
+?>
+
+<!DOCTYPE  html
+PUBLIC  "-//W3C//DTD  XHTML  1.0  Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html  xmlns="http://www.w3.org/1999/xhtml"  xml:lang="en"  lang="en">
+
+<head>
+<title>Set  Cookie</title>
+</head>
+
+<body>
+
+
+<?php
+
+//  Mendapatkan  nilai  cookie
+echo  $_COOKIE['nama_cookie'];
+?>
+
+
+<p>
+Tekan  Refresh  (F5);
+
+</body>
+
+</html>
+
